4092923121c81e5fe03c25c9850bc5910d8afec4
Eduardo Campos de Souza <eduardofidera@gmail.com>
2020-03-12 22:57:00 -0300

    Add test for App and move notes to components folder

---
+++ src/components/app/app.test.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./app"
+
+describe("App", function() {
+  it("Should render without crashing", function() {
+    const div = document.createElement("div")
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
