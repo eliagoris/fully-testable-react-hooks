355086ff48f8ca1dd8ee291bcab4361a543c9a70
Eduardo Campos de Souza <eduardofidera@gmail.com>
2020-03-13 12:39:44 -0300

    Add useNotes and initial form to view component

--- src/components/notes/notes.tsx
+++ src/components/notes/notes.tsx
@@ -1,10 +1,35 @@
 import React from "react"
 
+import { useNotes } from "./hooks/use-notes"
+
 export const Notes: React.FC = () => {
+  const { notes, handleAddNote } = useNotes()
+
+  function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const {
+      currentTarget: { elements }
+    } = e
+
+    const titleElement = elements.namedItem("title")
+    const value = (titleElement as HTMLInputElement).value
+
+    handleAddNote({ title: value })
+    e.currentTarget.reset()
+  }
+
   return (
     <>
-      <p>Notes</p>
-      Notes
+      <h1>Notes</h1>
+
+      {notes.map(({ id, title }) => (
+        <p key={id}>{title}</p>
+      ))}
+
+      <form data-testid="notes-form" onSubmit={handleFormSubmit}>
+        <input id="title" name="title" />
+        <button type="submit">add</button>
+      </form>
     </>
   )
 }
