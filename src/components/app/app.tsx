cc8cecd273aca951f81248833618167038a11f0f
Eduardo Campos de Souza <eduardofidera@gmail.com>
2020-03-13 14:25:56 -0300

    Init app component styles

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,40 @@
 import React from "react"
+import { createGlobalStyle } from "styled-components"
 
 import { Notes } from "../notes/notes"
+
+const GlobalStyle = createGlobalStyle`
+  *,
+	*:before,
+	*:after {
+ 		box-sizing: inherit;
+  }
+  
+  html, body {
+    display: flex;
+    min-height: 100%;
+  }
+
+  html {
+		box-sizing: border-box;
+  }
+
+	body {
+		margin: 0;
+		padding: 0;
+    font-family: Helvetica, Arial, sans-serif;
+    flex: 1 100%;
+  }
+
+  #root {
+    flex: 1 100%;
+  }
+`
 
 export const App: React.FC = () => {
   return (
     <>
+      <GlobalStyle />
       <Notes />
     </>
   )
